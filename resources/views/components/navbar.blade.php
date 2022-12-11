2c0f5e34f289beeeea27e13269b3da67c518381a
alyafitrin <alyafitrin@gmail.com>
2022-12-11 20:55:19 +0700

    add navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<head>
+@vite('resources/css/app.css')
+</head>
+<body>
+<div class="w-full h-1/6">
+    <div class="bg-teal-400  text-center py-8 text-3xl font-bold">
+        <h1>StreaMix</h1>
+    </div>
+</div>
+</body>
