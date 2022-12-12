ec32c65b27c56805ee29094922ec5d65157acf6e
Femi Novia Lina <77434812+FemiNoviaLina@users.noreply.github.com>
2022-12-13 00:18:20 +0700

    load dynamic data to dashboard view and create new sharing group form

--- app/Http/Controllers/SharingGroupController.php
+++ app/Http/Controllers/SharingGroupController.php
@@ -31,8 +31,16 @@ class SharingGroupController extends Controller
     }
 
     public function showDashboard() {
-        $sharingGroups = SharingGroup::all();
-
+        $platform = request()->query('platform', 'Netflix');
+        $sharingGroups = SharingGroup::leftJoin('users', 'sharing_groups.owner_id', '=', 'users.id')
+        ->leftJoin('group_members', 'group_members.group_id', '=', 'sharing_groups.id')
+        ->selectRaw('sharing_groups.id, sharing_groups.price, sharing_groups.packet, sharing_groups.quota, users.name, count(group_members.user_id) as member')
+        ->groupBy('sharing_groups.id')
+        ->groupBy('users.name')
+        ->where('sharing_groups.platform', '=', $platform)
+        ->havingRaw('count(group_members.user_id) <> 0')
+        ->get();
+        
         return view('dashboard', ['sharingGroups' => $sharingGroups]);
     }
 
